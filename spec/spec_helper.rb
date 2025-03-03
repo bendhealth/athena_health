d141f9fc5c19dcdfc774e2009e14ec9367376448
Leandro Costa <contato@leandrocosta.pro.br>
2025-03-03 14:53:28 -0300

    feat: add dotenv

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
 
+require 'dotenv/load'
 require 'athena_health'
 require 'vcr'
 
